e27aa18bd139231ba0e9cdbf5bcf639bc83b16a4
FlavioVillar <flavio_villar@hotmail.com>
2022-07-29 16:50:17 -0300

    feat: :sparkles: created endpoint get/ - all products

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -4,5 +4,6 @@ import * as productController from '../controllers/products.controller';
 const router = Router();
 
 router.post('/', productController.create);
+router.get('/', productController.getAll);
 
 export default router;
